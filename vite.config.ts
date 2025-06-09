03cd193c8b170dad4d800160cb01ba344c5e9b9c
Brandon C <brandonc@heyitsmass.dev>
2025-06-09 13:39:14 -0700

    updates vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	base: "/react-tooltip/",
+	build: {
+		outDir: "dist",
+		emptyOutDir: true,
+		rollupOptions: {
+			output: {
+				format: "es",
+				entryFileNames: "[name].js",
+				chunkFileNames: "[name].js",
+				assetFileNames: "[name].[ext]",
+			},
+		},
+	},
 });
